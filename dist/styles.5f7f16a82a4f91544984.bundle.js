01b450858fe3ecbdd86e2ee97fe0def244a27a26
Radoslav Mladenov <rvmladenov@gmail.com>
2016-11-29 15:26:32 +0200

    Changed the UI to use MDL instead 4

---
+++ dist/styles.5f7f16a82a4f91544984.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([2,3],{392:function(n,o){},393:function(n,o){},394:function(n,o){},943:function(n,o,c){c(393),c(394),n.exports=c(392)}},[943]);
+//# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
